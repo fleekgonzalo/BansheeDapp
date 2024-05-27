cc7214bf7f24e93bf1d7772d4888cb4d1ca52b21
Lamont V <lamontsprin@aol.com>
2024-05-27 08:36:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,63 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const app = express();
+import Footer from "./Global/Footer";
+import Header from "./Header/Header";
+import Loader from "./Global/Loader";
+import Sidebar from "./Sidebar/Sidebar";
+import Player from "./Global/Player";
+import Music from "./Home/Music";
+import Post from "./Home/Post";
+import Feed from "./Home/Feed";
+import RightSidebar from "./RightSidebar/RightSidebar";
+import Exchange from "./Global/Exchange";
+import Artists from "./Global/Artists";
+import PlayNow from "./Global/PlayNow";
+import Credit from "./Global/Credit";
+import ProfileMenu from "./Global/ProfileMenu";
+import Send from "./Global/Send";
+import TokenICO from "./Global/TokenICO";
+import PlaylistCard from "./Create/PlaylistCard";
+import Home from "./Home/Home";
+import Viral from "./Viral/Viral";
+import ViralCard from "./Viral/ViralCard";
+import Explore from "./Explore/Explore";
+import ExploreCard from "./Explore/ExploreCard";
+import Notifications from "./Notifications/Notifications";
+import NotificationPupup from "./Notifications/NotificationPupup";
+import ProfileEdit from "./ProfileEdit/ProfileEdit";
+import UserProfile from "./UserProfile/UserProfile";
+import CreateAccount from "./CreateAccount/CreateAccount";
+import CommentComp from "./Comment/CommentComp";
+import Contract from "./Global/Contract";
+import MobilePlay from "./Global/MobilePlay";
 
-const next = require("next");
-dotenv.config({ path: "./config.env" });
-const dev = process.env.NODE_ENV != "production";
-const nextServer = next({ dev });
-const handle = nextServer.getRequestHandler();
-
-const cookieParser = require("cookie-parser");
-const authRoute = require("./Api/routes/auth");
-const userRoute = require("./Api/routes/users");
-const postRoute = require("./Api/routes/posts");
-const commentRoute = require("./Api/routes/comments");
-const storyRoute = require("./Api/routes/stories");
-const conversationRoute = require("./Api/routes/conversations");
-const messageRoute = require("./Api/routes/messages");
-const path = require("path");
-const { errorHandler } = require("./Api/middlewares/error");
-const verifyToken = require("./Api/middlewares/verifyToken");
-
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("DB connection successful!"));
-
-app.use(express.json());
-app.use(cookieParser());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-app.use("/api/auth", authRoute);
-app.use("/api/user", verifyToken, userRoute);
-app.use("/api/post", verifyToken, postRoute);
-app.use("/api/comment", verifyToken, commentRoute);
-app.use("/api/story", verifyToken, storyRoute);
-app.use("/api/conversation", verifyToken, conversationRoute);
-app.use("/api/message", verifyToken, messageRoute);
-
-app.use(errorHandler);
-
-nextServer.prepare().then(() => {
-  app.get("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  app.listen(3000, () => {
-    console.log(`App running on port ${3000}....`);
-  });
-});
+export {
+  Footer,
+  Header,
+  Loader,
+  Sidebar,
+  Player,
+  Music,
+  Post,
+  Feed,
+  RightSidebar,
+  Exchange,
+  Artists,
+  PlayNow,
+  Credit,
+  ProfileMenu,
+  Send,
+  TokenICO,
+  PlaylistCard,
+  Home,
+  Viral,
+  ViralCard,
+  Explore,
+  ExploreCard,
+  Notifications,
+  NotificationPupup,
+  ProfileEdit,
+  UserProfile,
+  CreateAccount,
+  CommentComp,
+  Contract,
+  MobilePlay,
+};
